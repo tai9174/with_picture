748f5259ca5f96f2343b2113e84b8120ce118fec
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-06-23 08:45:19 +0900

    お気に入り機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
+  resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new,:create,:show]
   resources :pictures do
