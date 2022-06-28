21e05cce5f3b722512ce91323672061bfc4b688d
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-06-28 15:38:01 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'pictures#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   get 'favorites/index'
   get 'sessions/new'
