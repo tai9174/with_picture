1217b315fa342a1096b1b4828e8022b37c40b21a
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-06-28 15:29:19 +0900

    init

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'rexml'
-ruby '3.0.1'
+# ruby '3.0.1'
 gem 'faker'
 gem 'bcrypt'
 gem 'carrierwave'
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -69,14 +69,14 @@ class PicturesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_picture
-      @picture = Picture.find(params[:id])
-    end
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
 
     # Only allow a list of trusted parameters through.
-    def picture_params
-      params.require(:picture).permit(:image, :image_cache,:content)
-    end
+  def picture_params
+    params.require(:picture).permit(:image, :image_cache,:content)
+  end
 
 
 end
